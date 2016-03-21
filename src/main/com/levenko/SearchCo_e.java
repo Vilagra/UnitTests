867e2b0b356b07bd706f38126cda0fc91e4c909c
Vilagra <Vilagra@mail.ru>
2016-03-21 20:47:03 +0200

    Innitial commit

---
+++ src/main/com/levenko/SearchCo_e.java
@@ -0,0 +1,40 @@
+package com.levenko;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * Created by Vilagra on 17.03.2016.
+ */
+public class SearchCo_e {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Enter the path to the file");
+        Scanner in = new Scanner(System.in);
+        String pathFile = in.nextLine();
+        String allText = "";
+
+        try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(pathFile)))) {
+            String line;
+            while((line=fileReader.readLine())!=null) {
+                allText+=line;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        long start = System.currentTimeMillis();
+        System.out.println("Number of coincidence:"+countCode(allText,0));
+        long end = System.currentTimeMillis();
+        System.out.println("Algoritm worktime:" + (end-start));
+
+
+    }
+    public static int countCode(String str,int index) {
+        if (str.indexOf("co",index)==-1) return 0;
+        index =str.indexOf("co",index);
+        return str.substring(index+3,index+4).equals("e")?1+countCode(str,index+4):countCode(str,index+2);
+
+    }
+}
---
+++ src/test/com/levenko/SearchCo_eTest.java
@@ -0,0 +1,49 @@
+package com.levenko;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Vilagra on 21.03.2016.
+ */
+public class SearchCo_eTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testCountCode() throws Exception {
+        String s = "codecode";
+        int i = 2;
+
+        assertEquals(2,SearchCo_e.countCode(s,0));
+    }
+
+    @Test
+    public void testCountCode1() throws Exception {
+        String s = "";
+        int i = 0;
+
+        assertEquals(i,SearchCo_e.countCode(s,0));
+    }
+
+    @Test
+    public void testCountCode2() throws Exception {
+        String s = "codcode";
+        int i = 1;
+
+        assertEquals(i,SearchCo_e.countCode(s,0));
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testCountCode3() throws Exception {
+
+        SearchCo_e.countCode(null,5);
+
+    }
+}
